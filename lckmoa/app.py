dc3c2b3b5e0aa3a98ebab316d16a3e343fcb5034
kimbangmang <couxver2@gmail.com>
2021-03-14 18:01:24 +0900

    Update app.py

--- lckmoa/app.py
+++ lckmoa/app.py
@@ -13,7 +13,7 @@ def home():
 @app.route('/api/games', methods=['GET']) # 월별 경기 정보 보여주기
 def test_get():
     year = request.args.get('year_give', 2021)
-    month = request.args.get('month_give', 1)
+    month = request.args.get('month_give', 3)
     leagueCode = 'lck_2021_spring'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -21,6 +21,7 @@ def test_get():
     print(url)
     data = requests.get(url, headers=headers)
     result = data.json()  # json 데이터를 파이썬에서 다룰 수 있게 변환
+
     monthlyScheduleDailyGroup = result['monthlyScheduleDailyGroup']
     result = []
     for dailyGroup in monthlyScheduleDailyGroup:
@@ -42,8 +43,8 @@ def test_get():
                 'awayTeamScore': awayTeamScore,
             }
             result.append(doc)
-    #         print(gameDate, homeTeamName, homeTeamScore, awayTeamName, awayTeamScore)
-    # print(result)
+        #         print(gameDate, homeTeamName, homeTeamScore, awayTeamName, awayTeamScore)
+        # print(result)
     return jsonify({'result': 'success', 'data': result})
 
 
@@ -95,7 +96,7 @@ def news_get():
 
     newsList = soup.select('#main_pack > section > div > div.group_news > ul > li')
 
-    print(newsList)
+    # print(newsList)
     result = []
 
     for newsList in newsList:
@@ -128,7 +129,7 @@ def highlight_get():
     query = parse_qs(urlparse(url).query, keep_blank_values=True)
     playlist_id = query["list"][0]
 
-    print(f'get all playlist items links from {playlist_id}')
+    # print(f'get all playlist items links from {playlist_id}')
     youtube = googleapiclient.discovery.build("youtube", "v3", developerKey="AIzaSyAVDRkjd0KCHFUr5eZJuZXXs1TfG26hxIQ")
 
     request = youtube.playlistItems().list(
